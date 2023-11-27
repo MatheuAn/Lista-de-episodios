360f98f629e58d009d7037688e755c7385fd6b56
MatheuAn <130595852+MatheuAn@users.noreply.github.com>
2023-11-27 15:15:45 -0300

    listep.js

--- listep.js
+++ listep.js
@@ -1,3 +1,34 @@
+
+
+lista.forEach(item => {
+  if (item.image === "") {
+    item.image = imagemPadrao;
+  }
+});
+
+
+// Agora, você pode usar a lista no seu HTML com template strings
+const listaHTML = lista.map(item => `
+  <li onclick="toggleAssistido(this)">
+    <span><img class="imgpadraoep" src="${item.image}" /></span>
+    <a href="${item.link}" class="infoeppp">
+      <main class="infoe">
+        <h6 id="titlenameep">${item.titulo}</h6>
+        <h2 id="numbeppp">${item.episodio}</h2>
+      </main>
+      <div>
+        <h3 id="categoriaep">${item.idioma}</h3>
+        <h3>${item.time}</h3>
+        <h3></h3>
+      </div>
+    </a>
+  </li>
+`).join('');
+
+document.getElementById('listaEpisodios').innerHTML = listaHTML;
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var listaEpisodios = document.getElementById('listaEpisodios');
   var quantidadeLi = listaEpisodios.getElementsByTagName('li').length;
